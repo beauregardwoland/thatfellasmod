8db25260a36f0696e67be3209cbccaea059a618d
beauregardwoland <your-email@example.com>
2026-01-29 16:03:33 -0500

    made naval invvasions cross the board quicker fixed some focus oversight added small starter navy to manchu

--- common/defines/ship_xp_defines.lua
+++ common/defines/ship_xp_defines.lua
@@ -2,3 +2,7 @@ NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 0.0					-- XP cost for addin
 NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 0.0				-- XP cost for replacing one equipment module with an unrelated module when creating an equipment variant.
 NDefines.NProduction.EQUIPMENT_MODULE_CONVERT_XP_COST = 0.0				-- XP cost for converting one equipment module to a related module when creating an equipment variant.
 NDefines.NProduction.EQUIPMENT_MODULE_REMOVE_XP_COST = 0.0				-- XP cost for removing an equipment module and leaving the slot empty when creating an equipment variant.
+NDefines.NNavy.NAVAL_INVASION_PREPARE_DAYS = 10							-- base days needed to prepare a naval invasion
+NDefines.NNavy.NAVAL_INVASION_PLAN_CAP = 2									-- base cap of naval invasions can be planned at the same time
+NDefines.NNavy.BASE_NAVAL_INVASION_DIVISION_CAP = 8						-- base cap of divisions that can be assigned in a naval invasion
+NDefines.NNavy.NAVAL_DOMINANCE_STRIKE_FORCE_MULTIREGION_DECAY = 0.01    -- Percentage that the strike force mission's naval dominance multiplier decreases with for each additional assigned region
