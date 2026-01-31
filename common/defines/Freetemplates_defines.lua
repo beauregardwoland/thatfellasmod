da275de276f87eabe88753409bf1b936644a566c
beauregardwoland <your-email@example.com>
2026-01-31 05:21:41 -0500

    addeddecision based building eco for france aswell as beginings of set volounteer limit system

--- common/defines/Freetemplates_defines.lua
+++ common/defines/Freetemplates_defines.lua
@@ -1,3 +1,5 @@
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0
 NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0
+NDefines.NDiplomacy.VOLUNTEERS_PER_TARGET_PROVINCE = 0.0			-- Each province owned by the target country contributes this amount of volunteers to the limit.
+NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 0.0				-- Each army unit owned by the source country contributes this amount of volunteers to the limit.
